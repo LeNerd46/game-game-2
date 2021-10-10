767f138313dbe1296143973858b6bb8d080d4320
LeNerd46 <devon.shoutz@gmail.com>
2021-10-10 11:27:40 -0500

    Test
    Testing GitHub stuff

--- Assets/Scripts/Player/Leaning.cs
+++ Assets/Scripts/Player/Leaning.cs
@@ -19,33 +19,36 @@ public class Leaning : MonoBehaviour
         if (Input.GetMouseButton(1))
         {
             // Figure out which direction to lean in
-
-            if (Physics.Raycast(cam.transform.position, cam.transform.forward + new Vector3(number, 0), out RaycastHit hit, 3))
-            {
-                if (hit.collider == null)
-                    Debug.Log("Didn't hit anything!");
-                else
-                    curAngle = Mathf.MoveTowardsAngle(curAngle, 0f, rotationSpeed * Time.deltaTime);
-            }
-            else
-            {
-                Debug.Log("Didn't hit anything!");
-
-                curAngle = Mathf.MoveTowardsAngle(curAngle, -maxAngle, rotationSpeed * Time.deltaTime);
-            }
             
-            if (Physics.Raycast(cam.transform.position, cam.transform.forward + new Vector3(-number, 0), out RaycastHit _hit, 3))
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit oHit, 3))
             {
-                if (hit.collider == null)
-                    Debug.Log("Didn't hit anything!");
+                if (Physics.Raycast(cam.transform.position, cam.transform.forward + new Vector3(number, 0), out RaycastHit hit, 3))
+                {
+                    if (hit.collider == null)
+                        Debug.Log("Didn't hit anything!");
+                    else
+                        curAngle = Mathf.MoveTowardsAngle(curAngle, 0f, rotationSpeed * Time.deltaTime);
+                }
                 else
-                    curAngle = Mathf.MoveTowardsAngle(curAngle, 0f, rotationSpeed * Time.deltaTime);
-            }
-            else
-            {
-                Debug.Log("Didn't hit anything!");
+                {
+                    Debug.Log("Didn't hit anything!");
 
-                curAngle = Mathf.MoveTowardsAngle(curAngle, maxAngle, rotationSpeed * Time.deltaTime);
+                    curAngle = Mathf.MoveTowardsAngle(curAngle, -maxAngle, rotationSpeed * Time.deltaTime);
+                }
+
+                if (Physics.Raycast(cam.transform.position, cam.transform.forward + new Vector3(-number, 0), out RaycastHit _hit, 3))
+                {
+                    if (hit.collider == null)
+                        Debug.Log("Didn't hit anything!");
+                    else
+                        curAngle = Mathf.MoveTowardsAngle(curAngle, 0f, rotationSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    Debug.Log("Didn't hit anything!");
+
+                    curAngle = Mathf.MoveTowardsAngle(curAngle, maxAngle, rotationSpeed * Time.deltaTime);
+                }
             }
 
             cam.transform.localRotation = Quaternion.AngleAxis(curAngle, Vector3.forward);
